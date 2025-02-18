44fe3a020330a2b7b0ec6d17cd49535f959beb8d
e2203027 <e2203027@edu.vamk.fi>
2025-02-18 17:44:12 +0200

    added modules for math operations and tested import/export

---
+++ modules/main.js
@@ -0,0 +1,7 @@
+import { add, subtract } from '/modules/math.js';
+
+const num1 = 10;
+const num2 = 5;
+
+console.log(`Addition: ${add(num1, num2)}`); 
+console.log(`Subtraction: ${subtract(num1, num2)}`);
