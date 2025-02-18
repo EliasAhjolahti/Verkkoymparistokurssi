b9975bdcbfb2839cf73a0295a54e6c9f079d0da8
e2203027 <e2203027@edu.vamk.fi>
2025-02-18 17:58:37 +0200

    added functions for area calculation and number comparisons

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function calculateArea(radius){
+    const PI = 3.14
+    const area = PI * radius * radius
+}
+
+function compareNumbers(a,b){
+    if(a > b){
+        console.log(`${a} is greater than ${b}`);
+    }
+    else if(a < b){
+        console.log(`${b} is greater than ${a}`);
+    }
+    else{
+        console.log(`${a} and ${b} are equal`);
+    }
+}
+
+
+calculateArea(3);
+
+compareNumbers(1,2);
+compareNumbers(2,1);
+compareNumbers(1,1);
+
 
