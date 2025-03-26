b763e6fd912da74ea1493bfaa2fc44b834d10f20
e2203027 <e2203027@edu.vamk.fi>
2025-03-26 14:11:58 +0200

    active section ready

--- Refs_Portals_ContextAPI/smooth-scroll/src/App.jsx
+++ Refs_Portals_ContextAPI/smooth-scroll/src/App.jsx
@@ -1,31 +1,59 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 
 function App() {
 
   const homeRef = useRef(null);
   const aboutRef = useRef(null);
-  const contactRef = useRef(null)
+  const contactRef = useRef(null);
+  const [activeSection, setActiveSection] = useState('')
+
 
   return (
     <div>
 <nav>
-<button onClick={() => homeRef.current.scrollIntoView({ behavior: 'smooth' })}> go to home </button>
-<button onClick={() => aboutRef.current.scrollIntoView({ behavior: 'smooth' })}> go to about </button>
-<button onClick={() => contactRef.current.scrollIntoView({ behavior: 'smooth' })}> go to contact </button>
+
+<button
+  onClick={() => {
+    setActiveSection('home');
+    homeRef.current.scrollIntoView({ behavior: 'smooth' });
+  }}
+  className={activeSection === 'home' ? 'active' : ''}
+>
+  go to home
+</button>
+
+<button onClick={() => {
+  setActiveSection('about');
+  aboutRef.current.scrollIntoView({ behavior: 'smooth' });
+  }}
+  className={activeSection === 'about' ? 'active' : ''}
+  > 
+  go to about 
+  </button>
+
+  <button onClick={() => {
+  setActiveSection('contact');
+  aboutRef.current.scrollIntoView({ behavior: 'smooth' });
+  }}
+  className={activeSection === 'contact' ? 'active' : ''}
+  > 
+  go to contact
+  </button>
+
 </nav>
 
-<div id="home" ref={homeRef}>
+<div id="home" ref={homeRef} className={activeSection === 'home' ? 'active' : ''}>
 <h1>home</h1>
 <p>{'home '.repeat(300)}</p>
 </div>
 
-<div id="about" ref={aboutRef}>
+<div id="about" ref={aboutRef} className={activeSection === 'about' ? 'active' : ''}>
 <h1>about</h1>
 <p>{'about '.repeat(300)}</p>
 </div>
 
-<div id="contact" ref={contactRef}>
+<div id="contact" ref={contactRef} className={activeSection === 'contact' ? 'active' : ''}>
 <h1>contact</h1>
 <p>{'contact '.repeat(300)}</p>
 </div>
