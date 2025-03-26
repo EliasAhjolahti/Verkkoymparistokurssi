a4006e20acee759f7da75fd5bda05a172373a4aa
e2203027 <e2203027@edu.vamk.fi>
2025-03-26 13:40:30 +0200

    navigation ready

--- Refs_Portals_ContextAPI/smooth-scroll/src/App.jsx
+++ Refs_Portals_ContextAPI/smooth-scroll/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const contactRef = useRef(null)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+<nav>
+<button onClick={() => homeRef.current.scrollIntoView({ behavior: 'smooth' })}> go to home </button>
+<button onClick={() => aboutRef.current.scrollIntoView({ behavior: 'smooth' })}> go to about </button>
+<button onClick={() => contactRef.current.scrollIntoView({ behavior: 'smooth' })}> go to contact </button>
+</nav>
+
+<div id="home" ref={homeRef}>
+<h1>home</h1>
+<p>{'home '.repeat(300)}</p>
+</div>
+
+<div id="about" ref={aboutRef}>
+<h1>about</h1>
+<p>{'about '.repeat(300)}</p>
+</div>
+
+<div id="contact" ref={contactRef}>
+<h1>contact</h1>
+<p>{'contact '.repeat(300)}</p>
+</div>
+
+</div>
   )
 }
 
