b518daa762fa154c6e8cfb240590f9c4b4ca90cc
e2203027 <e2203027@edu.vamk.fi>
2025-03-31 22:08:18 +0300

    modal.jsx

--- HTTP_requests/Placepicker/src/App.jsx
+++ HTTP_requests/Placepicker/src/App.jsx
@@ -12,14 +12,10 @@ const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find((place) => place.
 );
 
 function App() {
-
-  const modal = useRef();
   const selectedPlace = useRef();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
-
-
-
 
 
   useEffect(() => {
@@ -37,12 +33,12 @@ function App() {
 
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -67,7 +63,7 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    setModalIsOpen(false);
 
     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
@@ -76,7 +72,7 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modalIsOpen}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
--- HTTP_requests/Placepicker/src/components/Modal.jsx
+++ HTTP_requests/Placepicker/src/components/Modal.jsx
@@ -1,22 +1,18 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { forwardRef, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+const Modal = forwardRef(function Modal({open, children }) {
   const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+
+  if (open){
+    dialog.current.showModal();
+  } else{
+    dialog.current.close();
+  }
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
+    <dialog className="modal" ref={dialog}  >
       {children}
     </dialog>,
     document.getElementById('modal')
