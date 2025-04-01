3334d7f669bec66387319f0214b39868bdde0ffd
e2203027 <e2203027@edu.vamk.fi>
2025-04-01 23:16:39 +0300

    delete button

--- budget_tracker_app/budget_tracker_app/src/App.jsx
+++ budget_tracker_app/budget_tracker_app/src/App.jsx
@@ -5,7 +5,14 @@ import TransactionList from './components/TransactionList'
 
 function App() {
 
+  const deleteTransaction = (indexDelete) => {
+    setTransactions(transactions.filter((_, index) => index !== indexDelete));
+  };
+  
+
 const [transactions, setTransactions] = useState([]);
+
+const saldo = transactions.reduce((acc, tx) => acc + tx.sum, 0)
 
 const addTransaction = (tx) => {
 setTransactions([...transactions, tx]);
@@ -13,10 +20,14 @@ setTransactions([...transactions, tx]);
 
   return (
     <>
+      <div className="balance-box">
+      <h2>Saldo: {saldo.toFixed(2)} €</h2>
+      </div>
       <TransactionForm onAdd={addTransaction}/>
-      <TransactionList transactions={transactions}/>    
+      <TransactionList transactions={transactions} onDelete={deleteTransaction}/>    
       </>
   )
 }
+
 
 export default App
--- budget_tracker_app/budget_tracker_app/src/components/TransactionList.jsx
+++ budget_tracker_app/budget_tracker_app/src/components/TransactionList.jsx
@@ -1,11 +1,13 @@
-export default function TransactionList({ transactions }) {
+export default function TransactionList({ transactions, onDelete }) {
+
     return (
       <div>
         <h3>Transactions</h3>
-        <ul>
+        <ul id="transaction-list">
           {transactions.map((tx, index) => (
-            <li key={index}>
+            <li key={index} className={tx.sum >= 0 ? 'income' : 'expense'}>
               {tx.description}: €{tx.sum}
+              <button onClick={() => onDelete(index)}>Poista</button>
             </li>
           ))}
         </ul>
