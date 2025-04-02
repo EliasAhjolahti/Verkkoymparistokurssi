26dcd35375e3fcc4ad98005d1bb398a07a985c7b
e2203027 <e2203027@edu.vamk.fi>
2025-04-02 22:33:31 +0300

    error message

--- HTTP_requests/FetchAndDisplay/components/UserList.jsx
+++ HTTP_requests/FetchAndDisplay/components/UserList.jsx
@@ -3,26 +3,38 @@ import { useEffect, useState } from "react";
 
 export default function UserList() {
 const [userData, setUserData] = useState([]);
+const [error, setError] = useState(null);
 
-
-useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((res) => res.json())
-      .then((data) => setUserData(data));
-  }, []);
-
-
-
-return( 
-<div>
-<ul>
-{userData.map(({ id, name, email}) => (
-    <li key={id}>
-        {name} - {email}
-    </li>
-))}
-
-</ul>
-</div>
-);
-}
+  
+    useEffect(() => {
+        const fetchUserData = async () => {
+          try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            if (!response.ok) {
+              throw new Error(`Fetch failed with status ${response.status}`);
+            }
+            const data = await response.json();
+            setUserData(data);
+          } catch (err) {
+            setError(err.message);
+          }
+        };
+    
+        fetchUserData();
+      }, []);
+    
+      if (error) return <p>Couldn't fetch data</p>;
+    
+      return (
+        <div>
+          <h2>User List</h2>
+          <ul>
+            {userData.map(({ id, name, email }) => (
+              <li key={id}>
+               {id} - {name} – {email}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
